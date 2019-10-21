ad0f8c3a94487d1996019e581dc4c47455d1560f
Olorunfemi Durosinmi <oldurosi@e1z3r7p21.42.us.org>
2019-10-21 05:17:23 -0700

    updating

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: oldurosi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 04:16:04 by oldurosi          #+#    #+#             */
-/*   Updated: 2019/10/21 04:56:59 by oldurosi         ###   ########.fr       */
+/*   Updated: 2019/10/21 05:16:59 by oldurosi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 int main(int argc, char **argv)
 {
+	int fd;
 	if (argc != 2)
 	{
-		ft_putstr("usage: ");
+		ft_putstr("usage: fillit input_file\n");
 		return (1);
 	}
-	if ()
+	if (fd = open(argv[1],O_RDONLY)) 
+	{
+	}
+	return (0);
 }
